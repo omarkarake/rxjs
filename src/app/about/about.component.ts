b0d7d3dd7a3b85db3ecd1a8d474cb4331fa56fd5
omar karake <omar.karake@amalitechtraining.org>
2024-07-16 13:01:30 +0200

    Observable Concatenation - In-Depth Explanation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import { fromEvent, interval, noop, Observable, Observer, timer } from "rxjs";
+import {
+  concat,
+  fromEvent,
+  interval,
+  noop,
+  Observable,
+  Observer,
+  of,
+  timer,
+} from "rxjs";
 import { createHttpObservable } from "../common/util";
 import { map } from "rxjs/operators";
 
@@ -9,10 +18,13 @@ import { map } from "rxjs/operators";
   styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-  counter: number | undefined;
   constructor() {}
   ngOnInit() {
-    
+    // const course1$ = of(1, 2, 3);
+    // const course1$ = interval(1000); // this means that course2$ and course3$ will not be executed, except if this interval is stopped
+    // const course2$ = of(4, 5, 6);
+    // const course3$ = of(7, 8, 9);
+    // const courses$ = concat(course1$, course2$, course3$)
+    // courses$.subscribe(console.log);
   }
 }
-
