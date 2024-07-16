a5ef4bdbee3ff5e56b41c3f15b619b017b3d45d2
omar karake <omar.karake@amalitechtraining.org>
2024-07-16 09:14:55 +0200

    learing about stream and observable

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { fromEvent, interval, timer } from "rxjs";
+
+@Component({
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
+})
+export class AboutComponent implements OnInit {
+  counter: number | undefined;
+  constructor() {}
+
+  ngOnInit() {
+    // streams
+    // this give us callback hell
+
+    /*document.addEventListener("click", (e) => {
+      console.log(e);
+      setTimeout(() => {
+        console.log("finished...");
+        this.counter = 0;
+        setInterval(() => {
+          console.log(this.counter);
+          this.counter++;
+        }, 1000);
+      }, 3000);
+    });
+    --------------------------*/
+
+    // let's define a stream observable
+    const interval$ = interval(1000);
+    // timer(delay, interval) observable
+    const interval2$ = timer(3000, 1000);
+    // stream for click event observable
+    const click$ = fromEvent(document, 'click');
+
+    // interval$ is an observable
+    // interval2$.subscribe((val) => console.log("stream 1: ", val));
+    // interval2$.subscribe((val) => console.log("stream 2: ", val));
+    // click$.subscribe(event => console.log(event));
+  }
+}
