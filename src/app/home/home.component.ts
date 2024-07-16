8131df5cf657644ae0a2421c4a3ab0a32322af34
omar karake <omar.karake@amalitechtraining.org>
2024-07-16 12:23:11 +0200

    Sharing HTTP Responses with the shareReplay Operator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,8 +24,12 @@ export class HomeComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    const http$: Observable<Course[]> = createHttpObservable("/api/courses");
-    const courses$: Observable<Course[]> = http$.pipe(map((res) => Object.values(res["payload"])));
+    const http$ = createHttpObservable("/api/courses");
+    const courses$: Observable<Course[]> = http$.pipe(
+        tap(()=>console.log('http request sent')), // this rxjs operator help to update something outside of observable
+        map((res) => Object.values(res["payload"])),
+        shareReplay()//this rxjs operator help us to share our excution to other subscriber
+    );
     this.beginnerCourses$ = courses$.pipe(
       map((courses) =>
         courses.filter((course) => course.category == "BEGINNER")
