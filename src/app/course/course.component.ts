8ca9afcf044324977b6b1dc9e43d21e5b58021d1
omar karake <omar.karake@amalitechtraining.org>
2024-07-17 14:21:46 +0200

    Setting Up the Course Component, fetching course in single page and lessons in that course.

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -15,6 +15,7 @@ import {
 } from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import { createHttpObservable } from '../common/util';
 
 
 @Component({
@@ -39,9 +40,10 @@ export class CourseComponent implements OnInit, AfterViewInit {
     ngOnInit() {
 
         const courseId = this.route.snapshot.params['id'];
-
-
-
+        this.course$ = createHttpObservable(`/api/courses/${courseId}`)
+        this.lessons$ = createHttpObservable(`/api/lessons?courseId=${courseId}&pageSize=100`).pipe(
+            map((response)=> response['payload'])
+        )
     }
 
     ngAfterViewInit() {
