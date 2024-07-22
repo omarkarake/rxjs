a2b2f8fd2419bde508a9a551ca36c2e4d0f917ba
omar karake <omar.karake@amalitechtraining.org>
2024-07-22 10:34:01 +0200

    The RxJs Debug Operator - Implementation Conclusion

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -25,6 +25,7 @@ import { merge, fromEvent, Observable, concat } from "rxjs";
 import { Lesson } from "../model/lesson";
 import { createHttpObservable } from "../common/util";
 import { searchLessons } from "../../../server/search-lessons.route";
+import { debug, RxJsLoggingLevel, setRxJsLoggingLevel } from "../common/debug";
 
 @Component({
   selector: "course",
@@ -43,22 +44,26 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.courseId = this.route.snapshot.params["id"];
-    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
-    
+    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`).pipe(
+      debug(RxJsLoggingLevel.INFO, "course value ")
+    );
+    // now we call log trace if we want or debug using this function
+    setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
   }
 
   ngAfterViewInit() {
-      this.lessons$ = fromEvent<any>(this.input.nativeElement, "keyup")
-      .pipe(
-        map((e) => e.target.value),
-        startWith(''), // works similary as concat but with less codes, will triggle the loadLessons first
-        debounceTime(400),
-        distinctUntilChanged(),
-        switchMap((search) => this.loadLessons(search))
-      );
-    }
+    this.lessons$ = fromEvent<any>(this.input.nativeElement, "keyup").pipe(
+      map((e) => e.target.value),
+      startWith(""), // works similary as concat but with less codes, will triggle the loadLessons first
+      debug(RxJsLoggingLevel.TRACE, "search "),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap((search) => this.loadLessons(search)),
+      debug(RxJsLoggingLevel.DEBUG, "lessons value ")
+    );
+  }
 
-  loadLessons(search = ""):  Observable<Lesson[]> {
+  loadLessons(search = ""): Observable<Lesson[]> {
     return createHttpObservable(
       `/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${search}`
     ).pipe(map((response) => response["payload"]));
