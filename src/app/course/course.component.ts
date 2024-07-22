c60a4547e38c152789f6cf63fd96b5d6b499e3ca
omar karake <omar.karake@amalitechtraining.org>
2024-07-22 11:13:01 +0200

    The RxJs forkJoin Operator - In Depth Explanation and Practical Example

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -20,8 +20,9 @@ import {
   concatAll,
   shareReplay,
   mergeMap,
+  throttleTime,
 } from "rxjs/operators";
-import { merge, fromEvent, Observable, concat } from "rxjs";
+import { merge, fromEvent, Observable, concat, forkJoin } from "rxjs";
 import { Lesson } from "../model/lesson";
 import { createHttpObservable } from "../common/util";
 import { searchLessons } from "../../../server/search-lessons.route";
@@ -44,23 +45,32 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.courseId = this.route.snapshot.params["id"];
-    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`).pipe(
-      debug(RxJsLoggingLevel.INFO, "course value ")
-    );
+    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+    // .pipe(debug(RxJsLoggingLevel.INFO, "course value "));
     // now we call log trace if we want or debug using this function
-    setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
+    // setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
+  
+    this.lessons$ = this.loadLessons(); // Assign the lessons observable to this.lessons$
+    
+    // this will handling parallel http request, and each of them must complete
+    forkJoin([this.course$, this.lessons$])
+      .pipe(
+        tap(([course, lessons]) => {
+          console.log("course", course);
+          console.log("lessons", lessons);
+        })
+      )
+      .subscribe();
   }
+  
 
   ngAfterViewInit() {
-    this.lessons$ = fromEvent<any>(this.input.nativeElement, "keyup").pipe(
-      map((e) => e.target.value),
-      startWith(""), // works similary as concat but with less codes, will triggle the loadLessons first
-      debug(RxJsLoggingLevel.TRACE, "search "),
-      debounceTime(400),
-      distinctUntilChanged(),
-      switchMap((search) => this.loadLessons(search)),
-      debug(RxJsLoggingLevel.DEBUG, "lessons value ")
-    );
+    fromEvent<any>(this.input.nativeElement, "keyup")
+      .pipe(
+        map((e) => e.target.value),
+        throttleTime(500)
+      )
+      .subscribe(console.log);
   }
 
   loadLessons(search = ""): Observable<Lesson[]> {
