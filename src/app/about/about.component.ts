e2b0ac8d2f2be45b8ce12112430ac0bd3c5b39b5
omar karake <omar.karake@amalitechtraining.org>
2024-07-22 12:22:54 +0200

    BehaviorSubject In Detail - When to Use it and Why?

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,34 +1,42 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+  concat,
+  fromEvent,
+  interval,
+  noop,
+  observable,
+  Observable,
+  of,
+  timer,
+  merge,
+  Subject,
+  BehaviorSubject,
+} from "rxjs";
+import { delayWhen, filter, map, take, timeout } from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+  ngOnInit() {
+    const subject = new BehaviorSubject(0); // initial value
+    // subject can be observable or observer
+    const series$ = subject.asObservable();
+    // series$ will be an observable
+    series$.subscribe((val) => console.log("early sub: " + val));
 
-    ngOnInit() {
-        const subject = new Subject();
-        // subject can be observable or observer
-        const series$ = subject.asObservable();
-        // series$ will be an observable
-        series$.subscribe(console.log)
-        subject.next(1);
-        subject.next(2);
-        subject.next(3);
-        subject.complete();
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
 
-    }
+    // subject.complete(); // if set will not trigger the late sub
 
-
+    setTimeout(() => {
+        series$.subscribe(val => console.log('late sub: ' + val));
+        subject.next(4);
+    });
+  }
 }
-
-
-
-
-
-
