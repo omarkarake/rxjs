7bc82f1913b1b4a3df022480e858cef0b3584e93
omar karake <omar.karake@amalitechtraining.org>
2024-07-22 12:03:45 +0200

    What are RxJs Subjects? A Simple Explanation/ creating own observable using rxjs subject

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -12,7 +12,15 @@ import {createHttpObservable} from '../common/util';
 export class AboutComponent implements OnInit {
 
     ngOnInit() {
-
+        const subject = new Subject();
+        // subject can be observable or observer
+        const series$ = subject.asObservable();
+        // series$ will be an observable
+        series$.subscribe(console.log)
+        subject.next(1);
+        subject.next(2);
+        subject.next(3);
+        subject.complete();
 
     }
 
